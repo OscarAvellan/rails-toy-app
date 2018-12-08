864ccde263f466d75692994472f7eb4a3d31422b
Oscar Avellan <oscaravellan94@gmail.com>
2018-12-09 10:21:33 +1100

    feat: add verbose output

--- run_rails_app.sh
+++ run_rails_app.sh
@@ -13,8 +13,11 @@ ls -a
 echo "cd into toy-app/"
 cd ./toy_app
 
+echo "starting server in background"
 rails s > /dev/null &
 
+echo "sleeping for 10s"
 sleep 10
 
+echo "Server pid: $(cat tmp/pids/server.pid)"
 kill $(cat tmp/pids/server.pid)
